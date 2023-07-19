f11629a987fb2d10513c358afa37ffc4d84719d7
hnimtadd <minhdat15012002@gmail.com>
2023-07-19 10:29:13 +0700

    entities: change ingredientids type to string

--- services/entities/dish.go
+++ services/entities/dish.go
@@ -2,10 +2,10 @@ package entities
 
 type Dish struct {
 	BaseEntity
-	DishId      string `json:"dish_id,omiempty" db:"dish_id,omiempty"`
-	Ingredients []int  `json:"ingredients_id,omiempty" db:"ingredients_id,omiempty"`
-	Name        string `json:"name,omiempty" db:"name,omiempty"`
-	Description string `json:"description,omiempty" db:"description,omiempty"`
+	DishId      string   `json:"dish_id,omiempty" db:"dish_id,omiempty"`
+	Ingredients []string `json:"ingredients_id,omiempty" db:"ingredients_id,omiempty"`
+	Name        string   `json:"name,omiempty" db:"name,omiempty"`
+	Description string   `json:"description,omiempty" db:"description,omiempty"`
 }
 
 type Ingredient struct {
--- services/entities/order.go
+++ services/entities/order.go
@@ -2,6 +2,6 @@ package entities
 
 type Order struct {
 	BaseEntity
-	OrderId  string `json:"order_id,omitempty" db:"order_id,omitempty"`
-	DishesId []int  `json:"dishes_id,omiempty" db:"dishes_id,omiempty"`
+	OrderId  string   `json:"order_id,omitempty" db:"order_id,omitempty"`
+	DishesId []string `json:"dishes_id,omiempty" db:"dishes_id,omiempty"`
 }
